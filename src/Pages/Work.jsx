1637a5b5f6d785cf698d4e2cf840e5d3e0b20a24
Aditya <adityaaryan.m4@gmail.com>
2022-09-13 18:03:25 +0530

    add work sec

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import urls from "../data"
+import { Link } from 'react-router-dom';
+
+const Container = ({ url }) => {
+  return (
+    <div className="container">
+      <img src={url} alt="work" />
+      <div className="info">
+        <div className="icon"><GitHubIcon style={{ background: "transparent", fill: "black" }} /></div>
+        <Link to="https://karashop.netlify.app/"><div className="icon">
+          <SearchIcon style={{ background: "transparent", fill: "black" }} />
+        </div></Link>
+      </div>
+    </div>
+  )
+}
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div className="work">
+      {urls.map(item => <Container key={item.id} url={item.url} />)}
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,16 @@
+const urls = [
+    {
+        id:1,
+        url:"https://i.postimg.cc/GpK2rs0T/1.png"
+    },
+    {
+        id:2,
+        url:"https://i.postimg.cc/DZM7y921/2.png" 
+    },
+    {
+        id:3,
+        url:"https://i.postimg.cc/BvMXG7RG/3.png"
+    },
+]
+
+export default urls
